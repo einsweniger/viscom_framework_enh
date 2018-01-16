4e6f17aa5b2fc7da4cdcf186413f73eb1ff7ff3e
Sebastian Maisch <sebastian.maisch@uni-ulm.de>
2018-01-16 15:33:41 +0100

    I can haz compilation with MacOS.

--- src/enh/core/serialization_helper.cpp
+++ src/enh/core/serialization_helper.cpp
@@ -7,7 +7,9 @@
  */
 
 #include "serialization_helper.h"
-#include <filesystem>
+#ifndef __APPLE_CC__
+#include <experimental/filesystem>
+#endif
 #include <g3log/g3log.hpp>
 
 namespace viscom::enh {
@@ -15,6 +17,7 @@ namespace viscom::enh {
     BinaryIAWrapper::BinaryIAWrapper(const std::string& filename) :
         ArchiveWrapper{ filename }
     {
+#ifndef __APPLE_CC__
         if (IsValid()) {
             auto lastModTime = std::experimental::filesystem::last_write_time(filename);
             auto timestamp = std::chrono::duration_cast<std::chrono::seconds>(lastModTime.time_since_epoch()).count();
@@ -31,13 +34,16 @@ namespace viscom::enh {
                 << "Filename: " << GetBinFilename(filename) << std::endl
                 << "Description: File does not exist.";
         }
+#endif
     }
 
     BinaryOAWrapper::BinaryOAWrapper(const std::string& filename) :
         ArchiveWrapper{ filename }
     {
+#ifndef __APPLE_CC__
         auto lastModTime = std::experimental::filesystem::last_write_time(filename);
         auto timestamp = std::chrono::duration_cast<std::chrono::seconds>(lastModTime.time_since_epoch()).count();
         (*this)(cereal::make_nvp("timestamp", timestamp));
+#endif
     }
 }
